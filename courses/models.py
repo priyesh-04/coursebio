1c989457de17edd196b30efe3ef48fee285cfaec
priyesh_04 <priyesh.shukla070@gmail.com>
2020-02-17 19:45:38 +0530

    course-list.html redesigned and some new fields added in course and provider models

--- courses/models.py
+++ courses/models.py
@@ -53,11 +53,12 @@ pre_save.connect(subcategory_pre_save_receiver, sender=SubCategory)
 
 
 class Provider(models.Model):
-	title				= models.CharField(max_length=100)
-	image_url 			= models.URLField(null=True, blank=True)
-	slug       			= models.SlugField(null=True, unique=True, blank=True)
-	updated     		= models.DateTimeField(auto_now=True)
-	timestamp   		= models.DateTimeField(auto_now_add=True)
+	title				= models.CharField(max_length=100,)
+	image_url 			= models.URLField(null=True, blank=True,)
+	plan_details        = models.CharField(max_length=140, null=True, blank=True,)
+	slug       			= models.SlugField(null=True, unique=True, blank=True,)
+	updated     		= models.DateTimeField(auto_now=True,)
+	timestamp   		= models.DateTimeField(auto_now_add=True,)
 
 
 	def __str__(self):
@@ -81,13 +82,17 @@ class Course(models.Model):
 	category 		= models.ForeignKey(Category, on_delete=models.CASCADE,)
 	subcategory 	= models.ForeignKey(SubCategory, on_delete=models.CASCADE,)
 	provider 		= models.ForeignKey(Provider, on_delete=models.CASCADE,)
-	video_url 		= models.URLField(null=True, blank=True)
-	title			= models.CharField(max_length=200, unique=True)
+	image_url       = models.URLField(null=True, blank=True,)
+	video_url 		= models.URLField(null=True, blank=True,)
+	title			= models.CharField(max_length=200, unique=True,)
 	description		= models.TextField()
-	price			= models.DecimalField(max_digits=19, decimal_places=2)
-	slug    		= models.SlugField(null=True, unique=True, blank=True)
-	updated     	= models.DateTimeField(auto_now=True)
-	timestamp   	= models.DateTimeField(auto_now_add=True)
+	author          = models.CharField(max_length=120, null=True, blank=True,)
+	duration        = models.CharField(max_length=50, null=True, blank=True,)
+	trial			= models.CharField(max_length=50, null=True, blank=True,)
+	price			= models.DecimalField(max_digits=19, decimal_places=2,)
+	slug    		= models.SlugField(null=True, unique=True, blank=True,)
+	updated     	= models.DateTimeField(auto_now=True,)
+	timestamp   	= models.DateTimeField(auto_now_add=True,)
 
 	def __str__(self):
 		return str(self.title)
