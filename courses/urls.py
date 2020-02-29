b9cadfc891fd6281a6878af867e565cb1e57c63e
priyesh_04 <priyesh.shukla070@gmail.com>
2020-02-29 21:40:55 +0530

    some basic design changes and tooltip show only if not none done

--- courses/urls.py
+++ courses/urls.py
@@ -12,7 +12,7 @@ from .views import (
 urlpatterns = [
 	url(r'^search/$', SearchListView.as_view(), name='search'),
 	url(r'^$', HomePageView.as_view(), name='home'),
-	url(r'^allcourses$', AllCourseListView.as_view(), name='all-courses'),
+	url(r'^allcourses/$', AllCourseListView.as_view(), name='all-courses'),
 	url(r'^(?P<slug>[\w-]+)/(?P<slug2>[\w-]+)/$', CourseListView.as_view(), name='course-list'),
 	url(r'^(?P<slug>[\w-]+)/$', SubcategoryListView.as_view(), name='subcategory-list'),
 	url(r'^(?P<slug>[\w-]+)/courses/(?P<slug2>[\w-]+)/$', CourseDetailView.as_view(), name='course-detail'),
