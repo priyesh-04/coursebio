dd7691b60b1c9d30febdc7704fd3f67c788395c5
priyesh_04 <priyesh.shukla070@gmail.com>
2020-03-20 18:17:07 +0530

    urlpatterns added in main urls file, swiper minified version used now

--- coursebio/urls.py
+++ coursebio/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.conf.urls import url
 from django.conf.urls import include
@@ -35,3 +38,8 @@ urlpatterns = [
     url(r'^blog/', include(('blog.urls', 'blog'), namespace='posts')),
     url(r'^', include(('courses.urls', 'courses'), namespace='course')),
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
