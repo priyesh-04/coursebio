98d40a772261a28d644eddf2983fc5738d015011
priyesh_04 <priyesh.shukla070@gmail.com>
2020-04-02 18:14:35 +0530

    blog create and update feature deleted on site

--- blog/mixins.py
+++ blog/mixins.py
@@ -1,16 +1,16 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+# from django.contrib.auth.mixins import LoginRequiredMixin
 
-class AccessMixin(LoginRequiredMixin):
-    def dispatch(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            if request.user.is_superuser:
-                return super().dispatch(request, *args, **kwargs)
-            elif request.user.is_admin:
-                return super().dispatch(request, *args, **kwargs)
-            elif request.user.is_staff:
-                return super().dispatch(request, *args, **kwargs)
-            else:
-                return self.handle_no_permission()
-        else:
-            return self.handle_no_permission()
-        return super().dispatch(request, *args, **kwargs)
+# class AccessMixin(LoginRequiredMixin):
+#     def dispatch(self, request, *args, **kwargs):
+#         if request.user.is_authenticated:
+#             if request.user.is_superuser:
+#                 return super().dispatch(request, *args, **kwargs)
+#             elif request.user.is_admin:
+#                 return super().dispatch(request, *args, **kwargs)
+#             elif request.user.is_staff:
+#                 return super().dispatch(request, *args, **kwargs)
+#             else:
+#                 return self.handle_no_permission()
+#         else:
+#             return self.handle_no_permission()
+#         return super().dispatch(request, *args, **kwargs)
