5c794dad69cdd2bf1027f8130185699015977256
priyesh_04 <priyesh.shukla070@gmail.com>
2020-04-28 14:06:15 +0530

    new droplet ip assinged to allowed host

--- coursebio/settings/production.py
+++ coursebio/settings/production.py
@@ -26,7 +26,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.coursebio.com', '138.197.2.214',]
+ALLOWED_HOSTS = ['.coursebio.com', '159.89.44.209',]
 
 from coursebio.secret_keys.mailchimp_api import MAILCHIMP_API_KEY, MAILCHIMP_DATA_CENTER, MAILCHIMP_EMAIL_LIST_ID
 
