c656dfe008874e5e6d8cb4f35e9ce2072239d9b8
priyesh_04 <priyesh.shukla070@gmail.com>
2020-05-07 21:03:52 +0530

    create_post task created in tasks.py and some minor settings changed

--- coursebio/celery.py
+++ coursebio/celery.py
@@ -2,11 +2,12 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
+
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coursebio.settings')
 os.environ.setdefault('FORKED_BY_MULTIPROCESSING', '1')
 
-app = Celery('proj')
+app = Celery('coursebio')
 
 # Using a string here means the worker don't have to serialize
 # the configuration object to child processes.
@@ -19,32 +20,27 @@ app.autodiscover_tasks()
 
 from celery.schedules import crontab
 app.conf.beat_schedule = {
-    'add-every-minute-contrab': {
-        'task': 'udemy_api',
-        'schedule': crontab(),
-        'args': (),
-    },
+    # 'add-every-minute-contrab': {
+    #     'task': 'courses.tasks.create_post',
+    #     'schedule': crontab(),
+    #     'args': (),
+    # },
     # Executes every Monday morning at 7:30 a.m.
     'add-every-monday-morning': {
-        'task': 'udemy_api',
+        'task': 'courses.tasks.udemy',
         'schedule': crontab(hour=7, minute=30, day_of_week=1),
         'args': (),
     },
-    # 'add-every-5-seconds': {
-    #     'task': 'multiply_two_numbers',
-    #     'schedule': 5.0,
-    #     'args': (16, 16)
-    # },
     'add-every-5-seconds': {
-        'task': 'udemy_api',
+        'task': 'courses.tasks.create_post',
         'schedule': 5.0,
         'args': ()
     },
-    # 'add-every-30-seconds': {
-    #     'task': 'tasks.udemyapi',
-    #     'schedule': 30.0,
-    #     'args': ()
-    # },
+    'add-every-minute-contrab': {
+        'task': 'courses.tasks.udemy',
+        'schedule': crontab(),
+        'args': ()
+    },
 }
 
 @app.task(bind=True)
