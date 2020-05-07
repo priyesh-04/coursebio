5a7fb20df2e3f98fc15291afdf6d179a39575225
priyesh_04 <priyesh.shukla070@gmail.com>
2020-05-07 18:33:36 +0530

    celery.py, tasks.py files created local and base settings updated for celery integration

--- courses/views.py
+++ courses/views.py
@@ -86,52 +86,9 @@ class HomePageView(ListView):
 		# print(context,'con')
 		return context
 
-from .udemy import Udemy
-from accounts.models import MyUser
 class AllCourseListView(ListView):
 	model = Course
-	template_name = 'courses/course_list.html'
-
-	def get_context_data(self, *args, **kwargs):
-		context = super(AllCourseListView, self).get_context_data(*args, **kwargs)
-		
-		user = MyUser.objects.filter(id=1)[0]
-		category = Category.objects.filter(id=3)[0]
-		subcategory = SubCategory.objects.filter(id=1)[0]
-		provider=Provider.objects.filter(id=1)[0]
-
-		udemy = Udemy('A3NMCg6nzLUPjTYR2bIjYeU9cUW1k3wHRQyzTX03', '9PPNGtZy0SPqcLn2mfIltOFzOkqzPQRInEmlLQAtscDkEjadVCO0wwc1Cu2qxMwt4ZlhlMcUZibyIrRx45pWeJmb7KJK4bCt6HgGAolEXDZA94VRpUmkYxWTFMbhB69f')
-		
-		udemy_course_list = udemy.courses(page=1, page_size=7,category='Lifestyle')
-		for i in range(len(udemy_course_list['results'])):
-			
-			udemy_course_detail = udemy.course_detail(udemy_course_list['results'][i]['id'], course ='@all')
-			course_ = Course.objects.filter(title=udemy_course_detail['title'])
-
-			if course_:
-				i=i+1
-				continue
-			
-			elif not course_:
-				print('Total',i,'courses added in database.')
-				course_obj = Course(user=user, category=category, provider=provider,title=udemy_course_detail['title'],description=udemy_course_detail['description'],)
-				course_obj.save()
-				d = udemy_course_detail['course_has_labels']
-
-				for i in range(len(d)):
-
-					subcategory = SubCategory.objects.filter(title=d[i]['label']['title'])
-					if not subcategory:
-						new_subcat = SubCategory.objects.create(category=category,title=d[i]['label']['title'])
-						course_obj.subcategory.add(new_subcat)
-					else:
-						course_obj.subcategory.add(subcategory[0])
-
-			else:
-				print('All courses already exists in our database.')
-
-		return context
-		
+	template_name = 'courses/course_list.html'		
 
 
 class SubcategoryListView(ListView):
