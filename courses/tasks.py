b76bcdcb5f097d4ce47a83259d593ca7737e06dd
priyesh_04 <priyesh.shukla070@gmail.com>
2020-05-10 09:21:09 +0530

    title key error exception handled using try except in tasks.py

--- courses/tasks.py
+++ courses/tasks.py
@@ -28,7 +28,10 @@ def udemy():
 			for i in range(len(udemy_course_list['results'])):
 				
 				udemy_course_detail = udemy.course_detail(udemy_course_list['results'][i]['id'], course ='@all')
-				course_ = Course.objects.filter(title=udemy_course_detail['title'])
+				try:
+					course_ = Course.objects.get(title=udemy_course_detail['title'])
+				except Exception as e:
+					print(e,'Exception')
 				# print('Detail',udemy_course_detail,'Detail')
 				if course_:
 					i=i+1
@@ -36,35 +39,38 @@ def udemy():
 				
 				elif not course_:
 					# print('Total',i,'courses added in database.')
-					category = Category.objects.get(title=my_cat_dict[udemy_cats[cats]])
-					image = udemy_course_detail['image_480x270']
-					author = udemy_course_detail['visible_instructors'][0]['title']
-					duration = udemy_course_detail['content_info']
-					level = udemy_course_detail['instructional_level']
-					url = 'https://www.udemy.com/' + udemy_course_detail['url']
-					course_obj = Course(user=user, category=category, provider=provider, image_url=image, title=udemy_course_detail['title'], description=udemy_course_detail['description'], author=author, duration=duration, level=level, course_url=url)
-					
 					try:
-						video = udemy_course_detail['promo_asset']['download_urls']['Video'][0]['file']
-						course_obj.video_url = video
+						category = Category.objects.get(title=my_cat_dict[udemy_cats[cats]])
+						image = udemy_course_detail['image_480x270']
+						author = udemy_course_detail['visible_instructors'][0]['title']
+						duration = udemy_course_detail['content_info']
+						level = udemy_course_detail['instructional_level']
+						url = 'https://www.udemy.com/' + udemy_course_detail['url']
+						course_obj = Course(user=user, category=category, provider=provider, image_url=image, title=udemy_course_detail['title'], description=udemy_course_detail['description'], author=author, duration=duration, level=level, course_url=url)
+						
+						try:
+							video = udemy_course_detail['promo_asset']['download_urls']['Video'][0]['file']
+							course_obj.video_url = video
+						except Exception as e:
+							print(e,'Exception')
+						if udemy_course_detail['is_paid']:
+							course_obj.price = 13.0
+						else:
+							course_obj.is_free=True
+						course_obj.certificate = True
+						course_obj.save()
+						d = udemy_course_detail['course_has_labels']
+
+						for j in range(len(d)):
+
+							subcategory = SubCategory.objects.get(title=d[j]['label']['title'])
+							if not subcategory:
+								new_subcat = SubCategory.objects.create(category=category,title=d[j]['label']['title'])
+								course_obj.subcategory.add(new_subcat)
+							else:
+								course_obj.subcategory.add(subcategory[0])
 					except Exception as e:
 						print(e,'Exception')
-					if udemy_course_detail['is_paid']:
-						course_obj.price = 13.0
-					else:
-						course_obj.is_free=True
-					course_obj.certificate = True
-					course_obj.save()
-					d = udemy_course_detail['course_has_labels']
-
-					for j in range(len(d)):
-
-						subcategory = SubCategory.objects.filter(title=d[j]['label']['title'])
-						if not subcategory:
-							new_subcat = SubCategory.objects.create(category=category,title=d[j]['label']['title'])
-							course_obj.subcategory.add(new_subcat)
-						else:
-							course_obj.subcategory.add(subcategory[0])
 			
 
 	updated_courses_count = Course.objects.filter(provider__title='udemy').count()
