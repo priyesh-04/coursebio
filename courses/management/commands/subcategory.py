e73e025f3b4a4982abb54599ba24fcaaefd2f11f
priyesh_04 <priyesh.shukla070@gmail.com>
2020-05-11 22:45:13 +0530

    Photography category appended in home page And Subcategory command created

---
+++ courses/management/commands/subcategory.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand, CommandError
+from courses.models import Course, Category, SubCategory
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+    	_cats = ['Computer Science', 'Arts & Design', 'Data Science', 'Business', 'Finance & Accounting', 'Health & Fitness', 'IT & Software', 'Lifestyle', 'Marketing', 'Music', 'Office Productivity', 'Personal Development', 'Photography', 'Teaching & Academics',]
+
+    	for i in range(len(_cats)):
+    		print(_cats[i])
+    		cat = Category.objects.filter(title=_cats[i])
+    		print(cat,'hhh')
+    		if cat:
+	    		courses = Course.objects.filter(category=cat[0])
+	    		for j in range(len(courses)):
+	    			sub_cat = SubCategory.objects.filter(course__title=courses[j].title)
+	    			for k in range(len(sub_cat)):
+	    				SubCategory.objects.filter(title=sub_cat[k].title).update(category=cat[0])
+
+    	return 'success'
