cf679741b7945c7d6bfe83d2231bdafd7586a3b7
priyesh_04 <priyesh.shukla070@gmail.com>
2020-05-13 12:00:41 +0530

    Topic model created, Course models certificate field changed to has_certificate

--- courses/models.py
+++ courses/models.py
@@ -7,28 +7,28 @@ from .utils import unique_slug_generator
 
 from django.utils.safestring import mark_safe
 from markdown_deux import markdown
-# Create your models here.
 
 
 class CategoryManager(models.Manager):
     def sorted_categories(self):
-    	qs = Category.objects.all()
-    	categories = []
-    	categories.append(qs.get(title='Computer Science'))
-    	categories.append(qs.get(title='Business'))
-    	categories.append(qs.get(title='Data Science'))
-    	categories.append(qs.get(title='IT & Software'))
-    	categories.append(qs.get(title='Marketing'))
-    	categories.append(qs.get(title='Office Productivity'))
-    	categories.append(qs.get(title='Personal Development'))
-    	categories.append(qs.get(title='Finance & Accounting'))
-    	categories.append(qs.get(title='Music'))
-    	categories.append(qs.get(title='Arts & Design'))
-    	categories.append(qs.get(title='Photography'))
-    	categories.append(qs.get(title='Health & Fitness'))
-    	categories.append(qs.get(title='Lifestyle'))
-    	categories.append(qs.get(title='Teaching & Academics'))
-    	return categories
+    	pass
+    # 	qs = Category.objects.all()
+    # 	categories = []
+    # 	categories.append(qs.get(title='Computer Science'))
+    # 	categories.append(qs.get(title='Business'))
+    # 	categories.append(qs.get(title='Data Science'))
+    # 	categories.append(qs.get(title='IT & Software'))
+    # 	categories.append(qs.get(title='Marketing'))
+    # 	categories.append(qs.get(title='Office Productivity'))
+    # 	categories.append(qs.get(title='Personal Development'))
+    # 	categories.append(qs.get(title='Finance & Accounting'))
+    # 	categories.append(qs.get(title='Music'))
+    # 	categories.append(qs.get(title='Arts & Design'))
+    # 	categories.append(qs.get(title='Photography'))
+    # 	categories.append(qs.get(title='Health & Fitness'))
+    # 	categories.append(qs.get(title='Lifestyle'))
+    # 	categories.append(qs.get(title='Teaching & Academics'))
+    # 	return categories
 
 class Category(models.Model):
 	title				= models.CharField(max_length=100, unique=True)
@@ -46,8 +46,8 @@ class Category(models.Model):
 	def get_absolute_url(self):
 		return reverse("course:subcategory-list", kwargs={"slug":self.slug})
 
-	def get_home_api_absolute_url(self):
-		return reverse("course:home-api", kwargs={"slug":self.slug})
+	# def get_home_api_absolute_url(self):
+	# 	return reverse("course:home-api", kwargs={"slug":self.slug})
 
 
 def category_pre_save_receiver(sender, instance, *args, **kwargs):
@@ -78,6 +78,18 @@ def subcategory_pre_save_receiver(sender, instance, *args, **kwargs):
         instance.slug = unique_slug_generator(instance)
 
 pre_save.connect(subcategory_pre_save_receiver, sender=SubCategory)
+
+
+class Topic(models.Model):
+	title				= models.CharField(max_length=255, unique=True)
+	category			= models.ForeignKey(Category, on_delete=models.CASCADE,)
+	subcategory 		= models.ManyToManyField(SubCategory, blank=True,)
+	slug       			= models.SlugField(max_length = 255, null=True, unique=True, blank=True)
+	updated     		= models.DateTimeField(auto_now=True)
+	timestamp   		= models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return str(self.title)
 
 
 class Provider(models.Model):
@@ -112,6 +124,7 @@ class Course(models.Model):
 	user			= models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
 	category 		= models.ForeignKey(Category, on_delete=models.CASCADE,)
 	subcategory 	= models.ManyToManyField(SubCategory, blank=True,)
+	topic 			= models.ManyToManyField(Topic, blank=True,)
 	provider 		= models.ForeignKey(Provider, on_delete=models.CASCADE,)
 	image_url       = models.URLField(max_length=500, null=True, blank=True,)
 	video_url 		= models.URLField(max_length=500, null=True, blank=True,)
@@ -124,7 +137,7 @@ class Course(models.Model):
 	level			= models.CharField(max_length=100, null=True, blank=True,)
 	rating			= models.DecimalField(max_digits=4, decimal_places=1, null=True, blank=True,)
 	is_free			= models.BooleanField(default=False,)
-	certificate		= models.BooleanField(default=False,)
+	has_certificate	= models.BooleanField(default=False,)
 	course_url		= models.URLField(null=True, blank=True,)
 	slug    		= models.SlugField(max_length = 255, null=True, unique=True, blank=True,)
 	popular			= models.BooleanField(default=False,)
