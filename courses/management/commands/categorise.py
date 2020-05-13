d8c909ea970932789372ea3a0ad6402105329cba
priyesh_04 <priyesh.shukla070@gmail.com>
2020-05-13 14:26:26 +0530

    tasks.py script modified to update courses data, Topic model signal created for slug

--- courses/management/commands/categorise.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-from courses.tasks import udemy
-
-class Command(BaseCommand):
-
-    def handle(self, *args, **options):
-        udemy()
-        return 'success'
