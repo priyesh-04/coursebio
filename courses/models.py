c0bcc7a8cdbafa8f6d7c6a729d380a1755a7f0b4
priyesh_04 <priyesh.shukla070@gmail.com>
2020-06-24 18:26:23 +0530

    recursion error solved, related tags removed

--- courses/models.py
+++ courses/models.py
@@ -146,7 +146,7 @@ class Course(models.Model):
 
 	def get_markdown(self):
 		description = self.description
-		markdown_text = markdown(description)
+		# markdown_text = markdown(description)
 		# return mark_safe(markdown_text)
 		return description
 
