ca64a74a81db49a789665bdbf23f01b4798eea9a
priyesh_04 <priyesh.shukla070@gmail.com>
2021-03-08 21:45:13 +0530

    udemy_tasks2.py created and ignored in gitignore file and db settings setup changed for sqlite

--- coursebio/settings/production.py
+++ coursebio/settings/production.py
@@ -123,18 +123,18 @@ from coursebio.secret_keys.database import PASSWORD
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'coursebio',
-        'USER': 'priyesh',
-        'PASSWORD': PASSWORD,
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'coursebio',
+    #     'USER': 'priyesh',
+    #     'PASSWORD': PASSWORD,
+    #     'HOST': 'localhost',
+    #     'PORT': '',
+    # }
 }
 
 
